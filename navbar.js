4979195c981429795365498004c288983ebf691a
MAHESH KUMAR PRAJAPAT <78361107+Mahesh-211@users.noreply.github.com>
2025-02-25 23:38:46 +0530

    some monika's updates

--- navbar.js
+++ navbar.js
@@ -278,3 +278,38 @@ function startAutoAllianceSlide() {
 // Initialize auto-slide
 startAutoAllianceSlide();
 
+
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    autoplay: {
+        delay: 3000, 
+        disableOnInteraction: false, 
+      },
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      950: {
+        slidesPerView: 3,
+      },
+    },
+  });
